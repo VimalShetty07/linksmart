478f51723706523dd76eda133f16851ca47d0420
Vimal Shetty <shettyvimal07@gmail.com>
2024-07-10 09:14:29 +0530

    Added photo size and shape api

--- linksmart/api/views.py
+++ linksmart/api/views.py
@@ -3,6 +3,11 @@ from rest_framework.views import APIView
 from .models import Employee,Department,Manager
 from .serializers import EmployeeListSerializer,DepartmentListSerializer,ManagerListSerializer
 from rest_framework.response import Response
+from PIL import Image
+from rest_framework.parsers import MultiPartParser, FormParser
+
+import cv2 
+
 
 # Create your views here.
 class DepartmentListView(APIView):
@@ -52,3 +57,47 @@ class EmployeeAddView(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+        
+
+
+
+class PhotoUploadView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        image_file = request.FILES.get('image')
+        if not image_file:
+            return Response({"detail": "No file provided"}, status=400)
+        try:
+            img = Image.open(image_file)
+            width, height = img.size
+            size = width * height
+            data = {'size': size, 'width': width, 'height': height}
+            return Response(data, status=201)
+        except Exception as e:
+            return Response({"detail": str(e)}, status=400)
+
+
+
+
+        
+# class PhotoUploadView(APIView):
+#     def post(self, request):
+#         image_file = request.FILES['image']
+
+        # cv2
+        # img = cv2.imread("geeksforgeeks.png") 
+        # wid = img.shape[1] 
+        # hgt = img.shape[0] 
+        
+        # print(str(wid) + "x" + str(hgt))
+        # with Image.open(image_file) as img:
+        #     size = img.size[0] * img.size[1]  # Width * Height in pixels
+        #     width, height = img.size
+
+            # No need to save the image, return data directly
+            # data = {'size': size, 'width': width, 'height': height}
+        # img = Image.open(image_file) 
+        # wid, hgt = img.size 
+        # print(wid,hgt)
+        # return Response("data", status=201)
